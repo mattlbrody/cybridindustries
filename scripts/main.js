bf1f0628696b2349a205c8da69c2e8d6b12b2502
mattbrodzz <mattlbrody@gmail.com>
2016-09-26 18:56:49 -0700

    major updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,22 @@
 $(function(e){"use strict";e(".animsition").animsition(),e(".nav-col").matchHeight(),e(".counter").counterUp({delay:10,time:1e3}),e(".owl-carousel-single").owlCarousel({loop:!0,navRewind:!1,margin:10,dots:!0,nav:!1,autoplay:!1,navText:[],items:1}),Swiper(".swiper-container",{loop:!0,pagination:".swiper-pagination",paginationClickable:!0,nextButton:".swiper-button-next",prevButton:".swiper-button-prev"}),e(".swiper-slide","#swiper").each(function(){var o=e(this).data("slide-img");e(this).css("background-image","url("+o+")")}),e("[data-toggle='tooltip']").tooltip();var o={delay:200,duration:600,distance:"60px",easing:"ease-in-out",rotate:{z:0},scale:1};window.sr=ScrollReveal(),sr.reveal(".one-step",o,500),e("#animated-text").textillate({loop:!0}),e(window).on("scroll resize",function(){return e(window).scrollTop()>=75?void e("body").addClass("body-scrolled"):e("body").removeClass("body-scrolled")})});
 
-$(".subscribe-email").click(function(){
-    $('input[type=submit]').css("visibility", "visible");
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
 
-$("input").blur(function(){
-    $('input[type=submit]').css("visibility", "hidden");
+// add a listener to 'scroll' event
+document.addEventListener("scroll", function() {
+    // get the active element and call blur
+    document.activeElement.blur();
 });
