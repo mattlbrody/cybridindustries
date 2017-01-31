d7b476a81a344edf341426a2bdd69aa97c5d75d4
Kristina Traeger <kristina@traeger.co>
2017-01-31 12:24:34 -0800

    add prospect name for auto reply email

--- mail.php
+++ mail.php
@@ -1,12 +1,20 @@
 <?php
+// send reply to
 $to = filter_var(trim($_POST['contactemail']), FILTER_SANITIZE_EMAIL);
 
+// get prospect name from email address
+$name = substr($to, 0, strrpos($to, '@'));
+// capitalize
+$name = ucfirst($name);
+
+// link to schedule a call
 $link = "https://calendly.com/matt-cybrid/15min/";
 
+// subject of reply email
 $subject = "You need a clone army! We got clone armies!!!";
 
 // message
-$txt = "Hey!\n\n";
+$txt = "Hey " .$name. ",\n\n";
 $txt .= "This is Matt with Cybrid Industries.\n\n";
 $txt .= "How many clone armies can I put you down for!?!\n\n";
 $txt .= "Just kidding, let's set up an exploratory meeting to make sure this is right for you :)\n\n";
@@ -17,7 +25,7 @@ $txt .= "Matt\n";
 
 $mailheader = "From: Matt Brody <matt@cybridindustries.com> \r\n";
 $mailheader .= "Cc: kristina@cybridindustries.com \r\n";
-$mailheader .= "Bcc: matt@cybridindustries.com \r\n";
+$mailheader .= "Bcc: kristina.traeger@gmail.com \r\n";
 mail($to, $subject, $txt, $mailheader, "-fmatt@cybridindustries.com") or die("Error!");
 $home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . 'index.html';
   header('Location: ' . $home_url);
